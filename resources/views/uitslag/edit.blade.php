9900e7d264a9090fe5a4e1e483bd0f74422dfe06
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-04-11 11:27:12 +0200

    user story 2 af

--- resources/views/uitslag/edit.blade.php
+++ resources/views/uitslag/edit.blade.php
@@ -1,34 +1,25 @@
 {{-- filepath: resources/views/uitslag/edit.blade.php --}}
 @extends('layouts.app')
 
-@section('title', 'Uitslag Wijzigen')
+@section('title', 'Wijzig Uitslag')
 
 @section('content')
 <div class="container mt-4">
-    <h1 class="mb-4">Uitslag Wijzigen</h1>
+    <h1 class="mb-4">Wijzig Uitslag</h1>
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
+
+    {{-- Edit Form --}}
     <form method="POST" action="{{ route('uitslag.update', ['id' => $uitslag->UitslagId]) }}">
         @csrf
-        <div class="mb-3">
-            <label for="Naam" class="form-label">Naam</label>
-            <input type="text" class="form-control" id="Naam" value="{{ $uitslag->Naam }}" disabled>
-        </div>
+        @method('PUT')
+
         <div class="mb-3">
             <label for="Aantalpunten" class="form-label">Aantal punten</label>
-            <input type="number" class="form-control" id="Aantalpunten" name="Aantalpunten" value="{{ $uitslag->Aantalpunten }}" required>
+            <input type="number" class="form-control" id="Aantalpunten" name="Aantalpunten" value="{{ old('Aantalpunten', $uitslag->Aantalpunten) }}" required>
         </div>
+
         <button type="submit" class="btn btn-primary">Wijzigen</button>
-        <a href="{{ route('reservering.uitslagen', ['id' => $uitslag->SpelId]) }}" class="btn btn-secondary">Annuleren</a>
     </form>
 </div>
 @endsection
--- routes/uitslag.php
+++ routes/uitslag.php
@@ -5,3 +5,4 @@ Route::get('/uitslag', [UitslagController::class, 'index'])->name('uitslag.index
 Route::get('/uitslag/{id}/edit', [UitslagController::class, 'editUitslag'])->name('uitslag.edit');
 Route::post('/uitslag/{id}/update', [UitslagController::class, 'updateUitslag'])->name('uitslag.update');
 Route::get('/reservering/{id}/uitslagen', [UitslagController::class, 'showUitslagen'])->name('reservering.uitslagen');
+Route::put('/uitslag/{id}/update', [UitslagController::class, 'updateUitslag'])->name('uitslag.update');
