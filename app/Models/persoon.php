bcf4406622bb46d5f109b1de3091b5c88b9db953
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-04-11 11:53:14 +0200

    hoop dat de merge goed is gegaan

--- app/Models/persoon.php
+++ app/Models/persoon.php
@@ -9,12 +9,39 @@ class Persoon extends Model
 {
     use HasFactory;
 
+    protected $table = 'persoons';
+    public $timestamps = false;
+
     protected $fillable = [
-        'type_persoon',
-        'Voornaam',
-        'Tussenvoegsel',
-        'Achternaam',
-        'Roepnaam',
-        'IsVolwassen',
+        'type_persoon_id',
+        'voornaam',
+        'tussenvoegsel',
+        'achternaam',
+        'roepnaam',
+        'is_volwassen',
+        'is_active',
+        'opmerking',
     ];
+
+    protected $casts = [
+        'is_volwassen' => 'boolean',
+        'is_active' => 'boolean',
+        'datum_aangemaakt' => 'datetime',
+        'datum_gewijzigd' => 'datetime',
+    ];
+
+    public function typePersoon()
+    {
+        return $this->belongsTo(TypePersoon::class);
+    }
+
+    public function reserveringen()
+    {
+        return $this->hasMany(Reservering::class, 'persoon_id');
+    }
+
+    public function getFullNameAttribute()
+    {
+        return $this->voornaam . ($this->tussenvoegsel ? " {$this->tussenvoegsel}" : "") . " {$this->achternaam}";
+    }
 }
