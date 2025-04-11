74156d510d2604a34e1aa328dd50e3bc53d261b1
Dennis Oostrom <336914@student.mboutrecht.nl>
2025-04-11 14:04:03 +0200

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,16 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            TypePersoonSeeder::class,
+            PakketOptieSeeder::class,
+            OpeningsTijdSeeder::class,
+            BaanSeeder::class,
+            PersoonSeeder::class,
+            ReserveringStatusSeeder::class,
+            ReserveringSeeder::class,
+            ContactSeeder::class,
+            SpelSeeder::class,
+            UitslagSeeder::class,
         ]);
 
-        $this->call(ReserveringenTableSeeder::class);
-        $this->call(PersoonTableSeeder::class);
-        $this->call(TypePersoonTableSeeder::class);
-        $this->call(BaanTableSeeder::class);
+    
     }
 }
