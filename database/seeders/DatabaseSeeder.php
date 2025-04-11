aee777be7d85fe4d9f90331173e60159fcac47fc
Dennis Oostrom <336914@student.mboutrecht.nl>
2025-04-11 14:10:09 +0200

    merge conflict error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,19 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            TypePersoonSeeder::class,
-            PakketOptieSeeder::class,
-            OpeningsTijdSeeder::class,
-            BaanSeeder::class,
-            PersoonSeeder::class,
-            ReserveringStatusSeeder::class,
-            ReserveringSeeder::class,
-            ContactSeeder::class,
-            SpelSeeder::class,
-            UitslagSeeder::class,
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
 
-    
+        $this->call(ReserveringenTableSeeder::class);
+        $this->call(PersoonTableSeeder::class);
+        $this->call(TypePersoonTableSeeder::class);
+        $this->call(BaanTableSeeder::class);
     }
 }
