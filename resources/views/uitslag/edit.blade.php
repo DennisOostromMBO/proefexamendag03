be34acad0b297346cf235deca445f19566a52ad9
Dynamiteboem2 <danielvangrol2006@gmail.com>
2025-04-11 11:34:15 +0200

    nog mooi

--- resources/views/uitslag/edit.blade.php
+++ resources/views/uitslag/edit.blade.php
@@ -1,13 +1,22 @@
 {{-- filepath: resources/views/uitslag/edit.blade.php --}}
 @extends('layouts.app')
 
-@section('title', 'Wijzig Uitslag')
+@section('title', 'Detail Uitslag')
 
 @section('content')
 <div class="container mt-4">
-    <h1 class="mb-4">Wijzig Uitslag</h1>
+    <h1 class="mb-4">Detail Uitslag</h1>
 
-
+    {{-- Display Validation Errors --}}
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
     {{-- Edit Form --}}
     <form method="POST" action="{{ route('uitslag.update', ['id' => $uitslag->UitslagId]) }}">
@@ -15,7 +24,7 @@
         @method('PUT')
 
         <div class="mb-3">
-            <label for="Aantalpunten" class="form-label">Aantal punten</label>
+            <label for="Aantalpunten" class="form-label">Aantal punten:</label>
             <input type="number" class="form-control" id="Aantalpunten" name="Aantalpunten" value="{{ old('Aantalpunten', $uitslag->Aantalpunten) }}" required>
         </div>
 
